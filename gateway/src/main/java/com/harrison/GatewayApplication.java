4c0e10e29307b40f932689ebec7c52c3b09bee13
WangHS <hscs.wang@gmail.com>
2025-03-09 22:35:04 +0800

    init gateway

---
+++ gateway/src/main/java/com/harrison/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.harrison;
+
+/**
+ * @description GatewayApplication
+ * @author WangHS
+ * @date 2025/3/8 20:18
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WangHS
+ * @date 2025/3/8 20:18
+ */
+@SpringBootApplication
+public class GatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
